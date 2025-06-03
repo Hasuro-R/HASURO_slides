385193f12185c8ffd5ab978a96bdcc3b7ce72301
HASURO <rentaro170928@icloud.com>
2025-06-04 06:44:01 +0900

    fix: page meta

--- src/app/lt/progate250529/layout.tsx
+++ src/app/lt/progate250529/layout.tsx
@@ -1,7 +1,13 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  description: "Progate LT会 Sansan出張版スライド",
+  title: "Progate LT会 Sansan出張版スライド",
+  openGraph: {
+    title: "Progate LT会 Sansan出張版スライド",
+  },
+  twitter: {
+    title: "Progate LT会 Sansan出張版スライド",
+  },
 }
 
 export default function ProgateLT250529Layout({
